9cf29e76a96d5bdf346b4f3d362391383751b350
heyblood <hey-blood@foxmail.com>
2022-12-26 22:52:31 +0800

    MockApi class

--- Chapter2-InheritedWidget/lib/main.dart
+++ Chapter2-InheritedWidget/lib/main.dart
@@ -44,3 +44,16 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+class MockApi {
+  String? dateAndTime;
+
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+            const Duration(seconds: 1), () => DateTime.now().toIso8601String())
+        .then((value) {
+      dateAndTime = value;
+      return value;
+    });
+  }
+}
