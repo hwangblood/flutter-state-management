64af2ab2e9b316f7b3f75ffc5168181ae4af0d34
hwangblood <hwangblood@gmail.com>
2023-01-29 16:49:05 +0800

    HomePage

--- Chapter4-InheritedNotifier/lib/main.dart
+++ Chapter4-InheritedNotifier/lib/main.dart
@@ -7,16 +7,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      home: HomPage(),
+    );
+  }
+}
+
+class HomPage extends StatelessWidget {
+  const HomPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: Column(
+        children: [
+          Slider(
+            value: 0,
+            onChanged: (double value) {},
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.yellow,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+              ),
+            ].expandequally().toList(),
+          ),
+        ],
       ),
     );
   }
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandequally() => map(
+        (e) => Expanded(child: e),
+      );
 }
