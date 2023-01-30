d505e2c917fa2d1e8a5b953dc869504c4f0350f8
hwangblood <hwangblood@gmail.com>
2023-01-30 13:48:50 +0800

    Breadcrumb Class

--- Chapter5-Provider/testingprovider_course/lib/main.dart
+++ Chapter5-Provider/testingprovider_course/lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -7,15 +8,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return const MaterialApp(
+      title: 'Breadcrumb App',
+      home: HomePage(),
+    );
+  }
+}
+
+class BreadCrumb {
+  bool isActive;
+  final String name;
+  BreadCrumb({
+    required this.isActive,
+    required this.name,
+  });
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
       ),
     );
   }
