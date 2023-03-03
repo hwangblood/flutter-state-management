8de36a3282a5caaa4887ba4113625688510a50fc
hwangblood <hwangblood@gmail.com>
2023-03-03 09:35:52 +0800

    Chapter 6: useStream hook example

--- Chapter06-Hooks/testingflutterhooks_course/lib/main.dart
+++ Chapter06-Hooks/testingflutterhooks_course/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,16 +16,25 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+Stream<String> getTime = Stream.periodic(
+  const Duration(seconds: 1),
+  (_) => DateTime.now().toLocal().toString(),
+);
+
+class HomePage extends HookWidget {
   const HomePage({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    final datetime = useStream(getTime);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(datetime.data ?? 'Home Page'),
+      ),
+      body: const Center(
+        child: Text('useStream change the appbar title per second'),
       ),
     );
   }
