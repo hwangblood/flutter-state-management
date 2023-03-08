15720529d042df061ee86c75dc393cfb93dab23f
hwangblood <hwangblood@gmail.com>
2023-03-08 15:01:10 +0800

    Chapter 8: Finish testing of first bloc example

--- Chapter08-Bloc/firstblocexample_course/test/persons_bloc_test.dart
+++ Chapter08-Bloc/firstblocexample_course/test/persons_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bloc_test/bloc_test.dart';
+import 'package:testingbloc_course/bloc/bloc_actions.dart';
 import 'package:testingbloc_course/bloc/person.dart';
 import 'package:testingbloc_course/bloc/persons_bloc.dart';
 
@@ -21,19 +22,77 @@ Future<Iterable<Person>> mockedGetPersons2(String _) =>
     Future.value(mockedPersons2);
 
 void main() {
-  group('Testing bloc', () {
-    // Write our tests
-    late PersonsBloc bloc;
+  group(
+    'Testing bloc',
+    () {
+      // Write our tests
+      late PersonsBloc bloc;
 
-    // call it per test in this group
-    setUp(() {
-      bloc = PersonsBloc();
-    });
+      // call it per test in this group
+      setUp(() {
+        bloc = PersonsBloc();
+      });
 
-    blocTest<PersonsBloc, FetchResult?>(
-      'Test initial state',
-      build: () => bloc,
-      verify: (bloc) => bloc.state == null,
-    );
-  });
+      blocTest<PersonsBloc, FetchResult?>(
+        'Test initial state',
+        build: () => bloc,
+        verify: (bloc) => expect(
+          bloc.state == null,
+          true,
+        ),
+      );
+
+      // fetch mocked data (persons1) and compare it with FetchResult
+      blocTest(
+        'Mock retrieving persons from fist iterable',
+        build: () => bloc,
+        act: (bloc) {
+          bloc.add(
+            const LoadPersonAction(
+              url: 'bummy_url_1',
+              loader: mockedGetPersons1,
+            ),
+          );
+          bloc.add(
+            const LoadPersonAction(
+              url: 'bummy_url_1',
+              loader: mockedGetPersons1,
+            ),
+          );
+        },
+        expect: () => [
+          const FetchResult(
+              persons: mockedPersons1, isRetrievedFromCache: false),
+          const FetchResult(
+              persons: mockedPersons1, isRetrievedFromCache: true),
+        ],
+      );
+
+      // fetch mocked data (persons2) and compare it with FetchResult
+      blocTest(
+        'Mock retrieving persons from second iterable',
+        build: () => bloc,
+        act: (bloc) {
+          bloc.add(
+            const LoadPersonAction(
+              url: 'bummy_url_2',
+              loader: mockedGetPersons2,
+            ),
+          );
+          bloc.add(
+            const LoadPersonAction(
+              url: 'bummy_url_2',
+              loader: mockedGetPersons2,
+            ),
+          );
+        },
+        expect: () => [
+          const FetchResult(
+              persons: mockedPersons2, isRetrievedFromCache: false),
+          const FetchResult(
+              persons: mockedPersons2, isRetrievedFromCache: true),
+        ],
+      );
+    },
+  );
 }
