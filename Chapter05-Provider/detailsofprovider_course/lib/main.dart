030bc528183e69a407b9ac358619b398c07ded6c
hwangblood <hwangblood@gmail.com>
2023-03-14 13:42:11 +0800

    Chapter 5: BaseObject

--- Chapter05-Provider/detailsofprovider_course/lib/main.dart
+++ Chapter05-Provider/detailsofprovider_course/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,6 +15,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
+@immutable
+class BaseObject {
+  final String id;
+  final String lastUpdated;
+  BaseObject()
+      : id = const Uuid().v4(),
+        lastUpdated = DateTime.now().toIso8601String();
+
+  @override
+  bool operator ==(covariant BaseObject other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({
     super.key,
@@ -23,7 +39,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Material App Bar'),
+        title: const Text('Provider Details Example'),
       ),
       body: const Center(
         child: Text('Hello World'),
