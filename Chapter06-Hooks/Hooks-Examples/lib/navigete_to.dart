7c7014a42ef15aef408153621f3c6ac5a4f22efd
hwangblood <hwangblood@gmail.com>
2023-03-21 11:30:24 +0800

    Chapter 6: route page as full screen dialog

--- Chapter06-Hooks/Hooks-Examples/lib/navigete_to.dart
+++ Chapter06-Hooks/Hooks-Examples/lib/navigete_to.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void navigateTo(BuildContext context, Widget widget) {
   Navigator.push(
     context,
-    MaterialPageRoute(builder: (_) => widget),
+    MaterialPageRoute(
+      builder: (_) => widget,
+      // fullscreenDialog: true,
+    ),
   );
 }
