047e41aace2f427efedd03a5b9cfbb6edbe4aa33
hwangblood <hwangblood@gmail.com>
2023-03-27 14:29:36 +0800

    rxdart: Create Example

---
+++ Chapter10-RxDart/testingrxdart_course/android/app/src/main/kotlin/com/hwangblood/testingrxdart_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hwangblood.testingrxdart_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Chapter10-RxDart/testingrxdart_course/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'RxDart Course',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RxDart Course'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
