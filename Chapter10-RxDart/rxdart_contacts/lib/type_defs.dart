2320ea4a99e6e59464f0e2b496d7e7ce746c419f
hwangblood <hwangblood@gmail.com>
2023-04-12 20:01:05 +0800

    rxdart: type_def Callbacks for ContactsApp

---
+++ Chapter10-RxDart/rxdart_contacts/lib/type_defs.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+
+import 'models/contact.dart';
+
+typedef LogoutCallback = VoidCallback;
+
+typedef DeleteAccountCallback = VoidCallback;
+
+typedef GoBackCallbacl = VoidCallback;
+
+typedef LoginCallback = void Function(
+  String email,
+  String password,
+);
+
+typedef RegisterCallback = void Function(
+  String email,
+  String password,
+);
+
+typedef CreateContactCallback = void Function(
+  String firstName,
+  String lastName,
+  String phoneNumber,
+);
+
+typedef DeleteContactCallback = void Function(
+  Contact contact,
+);
