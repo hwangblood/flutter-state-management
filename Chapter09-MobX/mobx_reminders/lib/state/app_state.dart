72098ae101a72a8144b958e0857e7f0e405094f8
hwangblood <hwangblood@gmail.com>
2023-05-05 14:08:20 +0800

    mobx: Extension to Sort Reminders by isDone, createAt

--- Chapter09-MobX/mobx_reminders/lib/state/app_state.dart
+++ Chapter09-MobX/mobx_reminders/lib/state/app_state.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mobx_reminders/state/reminder.dart';
 
 abstract class _DocumentKeys {
   static const String text = 'text';
@@ -10,3 +11,27 @@ typedef LoginOrRegisterFunction = Future<UserCredential> Function({
   required String email,
   required String password,
 });
+
+extension ToInt on bool {
+  /// Convert false to 0, true to 1
+  int toInteger() => this ? 1 : 0;
+}
+
+extension Sorted on Iterable<Reminder> {
+  /// Sort Reminders by isDone and createAt
+  ///
+  /// Iterable.sort() example:
+  /// ```dart
+  /// final numbers = <String>['two', 'three', 'four'];
+  /// Sort from shortest to longest.
+  /// numbers.sort((a, b) => a.length.compareTo(b.length));
+  /// print(numbers); // [two, four, three]
+  /// ```
+  Iterable<Reminder> sorted() => [...this]..sort((a, b) {
+      final isDone = a.isDone.toInteger().compareTo(b.isDone.toInteger());
+      if (isDone != 0) {
+        return isDone;
+      }
+      return a.createAt.compareTo(b.createAt);
+    });
+}
