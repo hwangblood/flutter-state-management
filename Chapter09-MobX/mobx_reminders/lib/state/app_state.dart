777c42677608e2a08d683d7585d384480f166459
hwangblood <hwangblood@gmail.com>
2023-05-05 13:55:34 +0800

    mobx: define login/register function signature

--- Chapter09-MobX/mobx_reminders/lib/state/app_state.dart
+++ Chapter09-MobX/mobx_reminders/lib/state/app_state.dart
@@ -1,5 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 abstract class _DocumentKeys {
   static const String text = 'text';
   static const String createAt = 'create_at';
   static const String isDone = 'is_done';
 }
+
+typedef LoginOrRegisterFunction = Future<UserCredential> Function({
+  required String email,
+  required String password,
+});
