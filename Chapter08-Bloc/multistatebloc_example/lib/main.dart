0e4533a592139279ab1b484f76aea0297ecc8780
hwangblood <hwangblood@gmail.com>
2023-07-30 16:38:30 +0800

    bloc: Rename notes app

--- Chapter08-Bloc/multistatebloc_example/lib/main.dart
+++ Chapter08-Bloc/multistatebloc_example/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notea App with Bloc',
+      title: 'Multistate Notes',
       home: BlocProvider(
         create: (context) => AppBloc(
           loginApi: LoginApi.instance(),
